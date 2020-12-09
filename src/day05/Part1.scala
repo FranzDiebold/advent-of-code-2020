b4a638a3911532ed632083e3e1b145850f807dff
Franz Diebold <franz.diebold@tum.de>
2020-12-09 18:07:02 +0000

    Add solutions for day 5.

---
+++ src/day05/Part1.scala
@@ -0,0 +1,35 @@
+/**
+ * Advent of Code 2020
+ * https://adventofcode.com/2020/day/5
+ * Day 5: Binary Boarding
+ * Part 1
+ */
+
+package day05
+
+import scala.io.Source
+
+object Part1 {
+  def readBoardingPass(bspCode: String): Int = {
+      bspCode
+        .map({
+          case 'F' | 'L' => 0
+          case 'B' | 'R' => 1
+        })
+        .foldLeft(0)((id: Int, currentDigit: Int) => 2 * id + currentDigit)
+  }
+
+  def getHighestSeatId(fileName: String): Int = {
+      Source
+        .fromFile(fileName)
+        .getLines()
+        .map(readBoardingPass(_))
+        .max
+  }
+
+  def main(args: Array[String]): Unit = {
+    val fileName = "input.txt"
+    val highestSeatId: Int = getHighestSeatId(fileName)
+    println(s"The highest seat ID is ${highestSeatId}.")
+  }
+}
---
+++ src/day05/Part2.scala
@@ -0,0 +1,39 @@
+/**
+ * Advent of Code 2020
+ * https://adventofcode.com/2020/day/5
+ * Day 5: Binary Boarding
+ * Part 2
+ */
+
+package day05
+
+import scala.io.Source
+
+import day05.Part1.{readBoardingPass}
+
+object Part2 {
+  def findSeat(fileName: String): Option[Int] = {
+    val seatPair: Option[Seq[Int]] = Source
+      .fromFile(fileName)
+      .getLines()
+      .map(readBoardingPass(_))
+      .toSeq
+      .sorted
+      .sliding(2)
+      .find((neighbours: Seq[Int]) => neighbours(1) - neighbours(0) == 2)
+
+    seatPair match {
+      case Some(neighbours: Seq[Int]) => Some(neighbours(1) - 1)
+      case None => None
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val fileName = "input.txt"
+    val seatIdOption: Option[Int] = findSeat(fileName)
+    seatIdOption match {
+      case Some(seatId) => println(s"The seat ID is ${seatId}.")
+      case _ => println("Not seat ID found.")
+    }
+  }
+}
