775432775c7727e45c062168129eed4bfe6f911c
Franz Diebold <franz.diebold@tum.de>
2020-12-10 19:35:13 +0000

    Add solutions for day 6.

---
+++ src/day06/Part1.scala
@@ -0,0 +1,37 @@
+/**
+ * Advent of Code 2020
+ * https://adventofcode.com/2020/day/6
+ * Day 6: Custom Customs
+ * Part 1
+ */
+
+package day06
+
+import scala.io.Source
+
+object Part1 {
+  def splitIterator[T](iter: Iterator[T], separator: T) = new Iterator[Seq[T]] {
+    def hasNext = iter.hasNext
+    def next() = iter.takeWhile(_ != separator).toSeq
+  }
+
+  def getNumberOfQuestionsAnyone(fileName: String): Int = {
+    val fileLines: Iterator[String] = Source
+      .fromFile(fileName)
+      .getLines()
+
+    splitIterator(fileLines, "")
+      .map((lines: Seq[String]) => lines
+        .map(_.toSet)
+        .reduce(_ ++ _)
+      )
+      .map(_.size)
+      .sum
+  }
+
+  def main(args: Array[String]): Unit = {
+    val fileName = "input.txt"
+    val numberOfQuestions: Int = getNumberOfQuestionsAnyone(fileName)
+    println(s"The number of questions anyone answered with 'yes' is ${numberOfQuestions}.")
+  }
+}
