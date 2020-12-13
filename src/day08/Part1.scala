78589bcf41d67e9430b85c392f7639395df6535f
Franz Diebold <franz.diebold@tum.de>
2020-12-13 16:57:33 +0000

    Add solutions for day 8.

---
+++ src/day08/Part1.scala
@@ -0,0 +1,49 @@
+/**
+ * Advent of Code 2020
+ * https://adventofcode.com/2020/day/8
+ * Day 8: Handheld Halting
+ * Part 1
+ */
+
+package day08
+
+import scala.io.Source
+import scala.util.matching.Regex
+
+object Part1 {
+  def readProgram(fileName: String): IndexedSeq[Option[Tuple2[String, Int]]] = {
+    val instructionRegex: Regex = """(nop|acc|jmp) ([+-]\d+)""".r
+    Source
+      .fromFile(fileName)
+      .getLines()
+      .map({
+        case instructionRegex(operation: String, argument: String) => Some(operation, argument.toInt)
+        case _ => None
+      })
+      .toIndexedSeq
+  }
+
+  def runProgram(program: IndexedSeq[Option[Tuple2[String, Int]]], instructionPointer: Int = 0, accumulator: Int = 0, previousInstructions: Set[Int] = Set[Int]()): Option[Int] = {
+    if (previousInstructions.contains(instructionPointer)) {
+      return Some(accumulator)
+    }
+    program(instructionPointer) match {
+      case Some((operation: String, argument: Int)) => operation match {
+        case "nop" => runProgram(program, instructionPointer + 1, accumulator, previousInstructions + instructionPointer)
+        case "acc" => runProgram(program, instructionPointer + 1, accumulator + argument, previousInstructions + instructionPointer)
+        case "jmp" => runProgram(program, instructionPointer + argument, accumulator,  previousInstructions + instructionPointer)
+        case _ => None
+      }
+      case None => None
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val fileName = "input.txt"
+    val program: IndexedSeq[Option[Tuple2[String, Int]]] = readProgram(fileName)
+    runProgram(program) match {
+      case Some(programResult: Int) => println(s"The value in the accumulator is ${programResult}.")
+      case None => println("Something went wrong running the program.")
+    }
+  }
+}
---
+++ src/day08/Part2.scala
@@ -0,0 +1,62 @@
+/**
+ * Advent of Code 2020
+ * https://adventofcode.com/2020/day/8
+ * Day 8: Handheld Halting
+ * Part 2
+ */
+
+package day08
+
+import day08.Part1.{readProgram}
+
+object Part2 {
+  def runProgram(program: IndexedSeq[Option[Tuple2[String, Int]]], instructionPointer: Int = 0, accumulator: Int = 0, previousInstructions: Set[Int] = Set[Int]()): Option[Int] = {
+    if (instructionPointer == program.size) {
+        return Some(accumulator)
+    }
+
+    if (previousInstructions.contains(instructionPointer)) {
+      return None
+    }
+
+    program(instructionPointer) match {
+      case Some((operation: String, argument: Int)) => operation match {
+        case "nop" => runProgram(program, instructionPointer + 1, accumulator, previousInstructions + instructionPointer)
+        case "acc" => runProgram(program, instructionPointer + 1, accumulator + argument, previousInstructions + instructionPointer)
+        case "jmp" => runProgram(program, instructionPointer + argument, accumulator,  previousInstructions + instructionPointer)
+        case _ => None
+      }
+      case None => None
+    }
+  }
+
+  def findOutputOfTerminatingProgram(program: IndexedSeq[Option[Tuple2[String, Int]]], instructionIdxToChange: Int = 0): Option[Int] = {
+    if (instructionIdxToChange >= program.size) {
+        return None
+    }
+
+    val operationChangeMap = Map[String, String]("nop" -> "jmp", "jmp" -> "nop")
+    program(instructionIdxToChange) match {
+      case Some((operation: String, argument: Int)) => operationChangeMap.keySet.contains(operation) match {
+        case true => {
+          val updatedProgram = program.updated(instructionIdxToChange, Some(operationChangeMap(operation), argument))
+          runProgram(updatedProgram) match {
+            case Some(programResult: Int) => Some(programResult)
+            case None => findOutputOfTerminatingProgram(program, instructionIdxToChange + 1)
+          }
+        }
+        case _ => findOutputOfTerminatingProgram(program, instructionIdxToChange + 1)
+      }
+      case None => None
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val fileName = "input.txt"
+    val program: IndexedSeq[Option[Tuple2[String, Int]]] = readProgram(fileName)
+    findOutputOfTerminatingProgram(program) match {
+      case Some(programResult: Int) => println(s"The value of the accumulator after the program terminates is ${programResult}.")
+      case None => println("Could not find a terminating program.")
+    }
+  }
+}
